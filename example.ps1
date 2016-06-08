bd56a736ae4cb2c36684075ca2a49c530275926e
Luke M Griffith <lukemgriffith@gmail.com>
2016-06-08 20:43:18 +0100

    adding example

---
+++ example.ps1
@@ -0,0 +1,14 @@
+﻿
+
+$a = Template -Name "TestTemplate" -TemplateParameters @{ModuleName="TestModule";ModuleBody="function test {}"} -Items { 
+
+    
+    Directory -Name $ModuleName -Children { 
+
+
+
+        Document -Name "$ModuleName.psm1" -Body $ModuleBody
+    }
+
+
+}
