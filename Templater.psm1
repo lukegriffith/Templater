11c0ab47104d03264b35573649626a47b6bf77fb
Luke M Griffith <lukemgriffith@gmail.com>
2016-06-12 11:45:47 +0100

    changed loader

--- Templater.psm1
+++ Templater.psm1
@@ -2,6 +2,7 @@
 [psTemplater]::root = $PSScriptRoot
 
 Get-ChildItem -Path $PSScriptRoot\Functions, $PSScriptRoot\Keywords -Exclude "*tests.ps1" -Recurse | ForEach-Object {
+
     . $_.FullName
 }
 
